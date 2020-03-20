f59d9f11f797f036056e5be7bbe29cde4381c456
Zaida Escila Martinez Moreno <zaidaescilamartinezmoreno@gmail.com>
2020-03-20 03:22:21 -0400

    Updating the presentation file for fMRI

--- MRI_Analysis/Presentation_stims/fMRI_PlayStim_MRIsts.m
+++ MRI_Analysis/Presentation_stims/fMRI_PlayStim_MRIsts.m
@@ -1,7 +1,7 @@
 clear sound;
 
 %Loads the noise that will be reproduced the whole run
-noise = dir('*Noise_run*');
+noise = dir('*Noise_run*,mat');
 noise = noise.name;
 runNumb = noise(10);
 load(noise);
@@ -9,7 +9,7 @@ x = Gated_noiseVector(:);
     sound(x,44100);
 
 %Loads the audio to be played
-stim = dir('*SparsedRandTonesStimuli_run*');
+stim = dir('*SparsedRandTonesStimuli_run*.mat');
 stim = stim.name;
 load(stim);
 
@@ -17,26 +17,18 @@ load(stim);
 
 for cell = 1:rows
     
-    WaitingTime = KbTriggerWait(53);
+    WaitingTime = KbTriggerWait(53); %% Waits for the trigger given by the fMRI scanner in the form of a number 5 keyboard press.
     WaitingTime_List{cell} = [cell WaitingTime]; %#ok<SAGROW>
     
     y = TOTAL_MAT_file_reduced(cell,:);
     sound(y,44100);
     
-    %%function secs = KbTriggerWait(keyCode, deviceNumber)
-    
-    %%Add a silence. If the fMRI acquires just after the trigger, the
-    %%stimuli should be done with the silence before the tones.
-
 end
 
-WaitingTime_File = cell2mat(WaitingTime_List);
-
-
-
+WaitingTime_File = cell2mat(WaitingTime_List); %%Creates a txt file with the time between each trigger.
 
 dlmwrite(['SparsedRandTonesStimuli_WaitingTimeList_run' num2str(runNumb) '.txt'],WaitingTime_File,'delimiter','\t', 'precision', 9);
 
 fprintf('The run is finished.\n');
 
-clear sound;
+clear sound; %% Clears the noise that is longer than the stim.
